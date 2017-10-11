a63204348e1f597b94a6dbf260de1f3f1e052dcd
Israel <1886afc@gmail.com>
2017-10-11 16:48:23 -0400

    database working

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-//requiring express
+//requiring dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 //setting a variable to express 'app'
 const app = express();
 
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+//connecting to mongoose
+mongoose.connect('mongodb://localhost/brain', {
+    useMongoClient: true
+})
+.then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 //Index route
 app.get('/', (req, res) =>{
-    res.send('index');
+    const title = 'Welcome'
+    res.render('index',{
+        title: title
+    });
 });
 
 // About Route
 app.get('/about', (req, res) =>{
-    res.send('ABOUT');
+    res.render('ABOUT');
 });
 
 //port that the server will be listening on
