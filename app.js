2e0c2a36a5c1df7f7e20f0196ff37e07c551c961
Israel <1886afc@gmail.com>
2017-10-15 19:24:41 -0400

    added edit and delete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const bodyParser = require('body-parser');
 //setting a variable to express 'app'
 const app = express();
@@ -30,6 +31,10 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+
 //Index route*********************************************************************
 app.get('/', (req, res) =>{
     const title = 'Welcome';
@@ -45,12 +50,30 @@ app.get('/about', (req, res) =>{
 
 // Idea Index page
 app.get('/ideas', (req, res) => {
-    res.render('ideas/index');
+    Idea.find({})
+        .sort({date:'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas:ideas
+            });
+        });
 });
 
 //Add Idea Form
 app.get('/ideas/add', (req, res) =>{
     res.render('ideas/add');
+});
+
+//edit idea form
+app.get('/ideas/edit/:id', (req, res) =>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea =>{
+            res.render('ideas/edit', {
+                idea:idea
+            });
+        });
 });
 
 // Process form
@@ -85,6 +108,31 @@ app.post('/ideas', (req, res)=>{
     }
 });
 
+// Edit form process
+app.put('/ideas/:id', (req, res) =>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            // New Values
+            idea.title = req.body.title;
+            idea.details = req.body.details;
+
+            //saving the update
+            idea.save()
+                .then(idea =>{
+                    res.redirect('/ideas');
+                })
+        });
+});
+
+// Delete idea
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({_id:req.params.id})
+        .then(() => {
+        res.redirect('/ideas')
+        })
+});
 //port that the server will be listening on******************************************
 const port = 5000;
 
